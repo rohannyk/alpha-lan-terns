ab478baed3c15343579b5db435e1e22b989ddd3d
lovepreet88 <lovely.smart88@gmail.com>
2015-07-10 16:21:50 -0400

    Commit GameInstance.java

---
+++ src/edu/concordia/app/model/GameInstance.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.concordia.app.model;
+
+/**
+ * @author lovepreet
+ *
+ */
+public class GameInstance {
+
+	/**
+	 * 
+	 */
+	public GameInstance() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
