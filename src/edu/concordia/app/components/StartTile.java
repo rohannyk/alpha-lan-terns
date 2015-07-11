f70ba7da5293637510b842fb71233b465eae57d7
savpreet <savpreetkaur1989@gmail.com>
2015-07-10 20:07:37 -0400

    Committed Start tile class

--- src/edu/concordia/app/components/StartTile.java
+++ src/edu/concordia/app/components/StartTile.java
@@ -1,5 +1,27 @@
 package edu.concordia.app.components;
 
-public class StartTile {
 
+import java.util.Vector;
+/**
+ * @author Team E
+ * This program is used to create the Start Tile 
+ * This Tile is the first tile of the game
+ * The Red color in the tile chooses the first player to start the game
+ *
+ */
+public class StartTile {
+	
+  public Vector<String> edgecolour = new Vector<String>(4);
+  
+  {
+        edgecolour.addElement("RED");
+	    edgecolour.addElement("WHITE");
+	    edgecolour.addElement("BLUE");
+	    edgecolour.addElement("BLACK");
+	  
+  }
+  
+	
+	
 }
+	
