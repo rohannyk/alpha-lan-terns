6271b3f36a4a23a90d44d6f747a5f4e2758e0f4d
salehahmedca <sachchabd@gmail.com>
2015-07-12 20:50:30 -0400

    Updated FavorTokens

--- src/edu/concordia/app/components/FavorTokens.java
+++ src/edu/concordia/app/components/FavorTokens.java
@@ -2,7 +2,9 @@
  * 
  */
 package edu.concordia.app.components;
+
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 
 /**
  * This class creates Favor Tokens for the players to use in the game
@@ -11,54 +13,64 @@ import javax.xml.bind.annotation.XmlAttribute;
  */
 public class FavorTokens {
 	
-	public int favtoken = 0;
-	//public int favtoken = 20;
+	
+	public int favtokenCount = 0;
+	
+	//	public int favtoken = 20;
+	
 	//private static int gameFavorToken = 20;
+	
 	//private int playerFavorToken = 0;
+	
+	
 	/**
 	 * Default Constructor
 	 */
 	public FavorTokens(){
 		
 	}
+	
 	/**
 	 * Default Constructor
 	 */
-	public FavorTokens(int favtoken){
-		this.favtoken = favtoken;
+	public FavorTokens(int favtokenCount){
+		this.favtokenCount = favtokenCount;
 	}
 	
 	
 	//getter and setter for favor tokens
 	//variable playerFavorTokens initialized to 0
 	//private static gameFavorToken initialized to 20
+
+
+	@XmlAttribute(name="totalFavorTokens")
 	/**
 	 * @return the favtoken
 	 */
-	public int getFavtoken() {
-		return favtoken;
+	public int getFavtokenCount() {
+		return favtokenCount;
 	}
 
 
 	/**
 	 * @param favtoken the favtoken to set
 	 */
-	public void setFavtoken(int favtoken) {
-		this.favtoken = favtoken;
+	public void setFavtokenCount(int favtokenCount) {
+		this.favtokenCount = favtokenCount;
 	}
 	
 	/**
 	 * @param tokens The favor tokens added for players 
 	 */
 	public void addFavorToken(int tokens){
-		favtoken = favtoken + tokens; 
+		favtokenCount = favtokenCount + tokens; 
 	}
 	
 	/**
 	 * @param tokens The favor tokens added for players 
 	 */
 	public void removeFavorToken(int tokens){
-		favtoken = favtoken - tokens; 
+		favtokenCount = favtokenCount - tokens; 
 	}
 
 }
