f58499da0673b8e8b80f209327a039b5e62b84f6
salehahmedca <sachchabd@gmail.com>
2015-07-14 18:55:44 -0400

    Updated StartTile

--- src/edu/concordia/app/components/StartTile.java
+++ src/edu/concordia/app/components/StartTile.java
@@ -6,15 +6,14 @@ import java.util.Vector;
  * @author Team E This program is used to create the Start Tile This Tile is the
  *         first tile of the game The Red color in the tile chooses the first
  *         player to start the game
- *
  */
 public class StartTile {
 
-//	public Vector<String> edgecolour = new Vector<String>(4);
+	// vector for the colors for the start tile
 	private Vector<String> startTileColour;
 
 	/**
-	 * 
+	 * Constructor to set the values for the start tile
 	 */
 	public StartTile() {
 		startTileColour = new Vector<String>(4);
@@ -26,6 +25,7 @@ public class StartTile {
 	}
 
 	/**
+	 * getter for the vector for the start tile
 	 * @return the startTileColour
 	 */
 	public Vector<String> getStartTileColour() {
@@ -33,12 +33,11 @@ public class StartTile {
 	}
 
 	/**
+	 * setter for the vector for the start tile
 	 * @param startTileColour the startTileColour to set
 	 */
 	public void setStartTileColour(Vector<String> startTileColour) {
 		this.startTileColour = startTileColour;
 	}
 	
-	
-
 }
