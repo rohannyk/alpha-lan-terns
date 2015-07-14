b0a8c75936af0699231c7f87e96f4db500d50ce7
salehahmedca <sachchabd@gmail.com>
2015-07-14 18:53:56 -0400

    Updated LakeTiles

--- src/edu/concordia/app/components/LakeTiles.java
+++ src/edu/concordia/app/components/LakeTiles.java
@@ -3,32 +3,40 @@ package edu.concordia.app.components;
 import java.util.Vector;
 
 /**
- * @author abhijit
- *
+ * this class is to create the lake tile object, the lake tile have four colors
+ * and some of the lake tiles have a platform marker,again lake tile can have adjacent
+ * top/bottom/left/right lake tile 
+ * @author Team E
  */
 public class LakeTiles {
 
+	// default constructor
 	public LakeTiles() {
 
 	}
 	
-	private int tilesId;
-	private String topColor;
-	private String leftColor;
-	private String rightColor;
-	private String bottomColor;
-	private boolean platform;
-	private int topTileId;
-	private int bottomTileId;
-	private int leftTileId;
-	private int rightTileId;
+	private int tilesId; 			// variable for tile's id
+	private String topColor;		// variable for tile's top color
+	private String leftColor;		// variable for tile's left color
+	private String rightColor;		// variable for tile's right color
+	private String bottomColor;		// variable for tile's bottom color
+	private boolean platform;		// variable for tile's platform marker
+	private int topTileId;			// variable for tile's top adjacent tile's id
+	private int bottomTileId;		// variable for tile's bottom adjacent tile's id
+	private int leftTileId;			// variable for tile's left adjacent tile's id
+	private int rightTileId;		// variable for tile's right adjacent tile's id
 
 	/**
+	 * Constructor
 	 * @param topColor
 	 * @param leftColor
 	 * @param rightColor
 	 * @param bottomColor
 	 * @param platform
+	 * @param topTileId
+	 * @param bottomTileId
+	 * @param leftTileId
+	 * @param rightTileId
 	 */
 	public LakeTiles(int tilesId, String topColor, String leftColor, String rightColor,
 			String bottomColor, boolean platform, int topTileId, int bottomTileId, int leftTileId, int rightTileId) {
@@ -51,48 +59,55 @@ public class LakeTiles {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void initializeTiles() {
 
+		// set the values for vector for tile 1
 		Vector tile1 = new Vector(5);
 		tile1.addElement(new String("GREEN"));
 		tile1.addElement(new String("BLUE"));
 		tile1.addElement(new String("BLACK"));
 		tile1.addElement(new String("ORANGE"));
 		tile1.addElement(new Boolean(false));
-
+		
+		// set the values for vector for tile 2
 		Vector tile2 = new Vector(5);
 		tile2.addElement(new String("GREEN"));
 		tile2.addElement(new String("GREEN"));
 		tile2.addElement(new String("GREEN"));
 		tile2.addElement(new String("RED"));
 		tile2.addElement(new Boolean(false));
-
+		
+		// set the values for vector for tile 3
 		Vector tile3 = new Vector(5);
 		tile3.addElement(new String("RED"));
 		tile3.addElement(new String("BLACK"));
 		tile3.addElement(new String("GREEN"));
 		tile3.addElement(new String("Purple"));
 		tile3.addElement(new Boolean(false));
-
+		
+		// set the values for vector for tile 4
 		Vector tile4 = new Vector(5);
 		tile4.addElement(new String("WHITE"));
 		tile4.addElement(new String("BLACK"));
 		tile4.addElement(new String("ORANGE"));
 		tile4.addElement(new String("BLACK"));
 		tile4.addElement(new Boolean(false));
-
+		
+		// set the values for vector for tile 5
 		Vector tile5 = new Vector(5);
 		tile5.addElement(new String("ORANGE"));
 		tile5.addElement(new String("PURPLE"));
 		tile5.addElement(new String("ORANGE"));
 		tile5.addElement(new String("ORANGE"));
 		tile5.addElement(new Boolean(false));
-
+		
+		// set the values for vector for tile 6
 		Vector tile6 = new Vector(5);
 		tile6.addElement(new String("RED"));
 		tile6.addElement(new String("GREEN"));
 		tile6.addElement(new String("BLUE"));
 		tile6.addElement(new String("PURPLE"));
 		tile6.addElement(new Boolean(false));
-
+		
+		// set the values for vector for tile 7
 		Vector tile7 = new Vector(5);
 		tile7.addElement(new String("WHITE"));
 		tile7.addElement(new String("GREEN"));
@@ -100,27 +115,31 @@ public class LakeTiles {
 		tile7.addElement(new String("GREEN"));
 		tile7.addElement(new Boolean(true));
 
+		// set the values for vector for tile 8
 		Vector tile8 = new Vector(5);
 		tile8.addElement(new String("WHITE"));
 		tile8.addElement(new String("BLUE"));
 		tile8.addElement(new String("BLACK"));
 		tile8.addElement(new String("BLUE"));
 		tile8.addElement(new Boolean(true));
-
+		
+		// set the values for vector for tile 9
 		Vector tile9 = new Vector(5);
 		tile9.addElement(new String("BLUE"));
 		tile9.addElement(new String("PURPLE"));
 		tile9.addElement(new String("WHITE"));
 		tile9.addElement(new String("RED"));
 		tile9.addElement(new Boolean(false));
-
+		
+		// set the values for vector for tile 10
 		Vector tile10 = new Vector(5);
 		tile10.addElement(new String("RED"));
 		tile10.addElement(new String("GREEN"));
 		tile10.addElement(new String("BLACK"));
 		tile10.addElement(new String("RED"));
 		tile10.addElement(new Boolean(false));
-
+		
+		// set the values for vector for tile 11
 		Vector tile11 = new Vector(5);
 		tile11.addElement(new String("GREEN"));
 		tile11.addElement(new String("RED"));
@@ -128,6 +147,7 @@ public class LakeTiles {
 		tile11.addElement(new String("WHITE"));
 		tile11.addElement(new Boolean(false));
 
+		// set the values for vector for tile 12
 		Vector tile12 = new Vector(5);
 		tile12.addElement(new String("RED"));
 		tile12.addElement(new String("GREEN"));
@@ -135,6 +155,7 @@ public class LakeTiles {
 		tile12.addElement(new String("RED"));
 		tile12.addElement(new Boolean(true));
 
+		// set the values for vector for tile 13
 		Vector tile13 = new Vector(5);
 		tile13.addElement(new String("BLACK"));
 		tile13.addElement(new String("PURPLE"));
@@ -142,6 +163,7 @@ public class LakeTiles {
 		tile13.addElement(new String("BLUE"));
 		tile13.addElement(new Boolean(false));
 
+		// set the values for vector for tile 14
 		Vector tile14 = new Vector(5);
 		tile14.addElement(new String("ORANGE"));
 		tile14.addElement(new String("PURPLE"));
@@ -149,6 +171,7 @@ public class LakeTiles {
 		tile14.addElement(new String("WHITE"));
 		tile14.addElement(new Boolean(true));
 
+		// set the values for vector for tile 15
 		Vector tile15 = new Vector(5);
 		tile15.addElement(new String("GREEN"));
 		tile15.addElement(new String("BLUE"));
@@ -156,6 +179,7 @@ public class LakeTiles {
 		tile15.addElement(new String("BLUE"));
 		tile15.addElement(new Boolean(false));
 
+		// set the values for vector for tile 16
 		Vector tile16 = new Vector(5);
 		tile16.addElement(new String("WHITE"));
 		tile16.addElement(new String("WHITE"));
@@ -163,13 +187,15 @@ public class LakeTiles {
 		tile16.addElement(new String("GREEN"));
 		tile16.addElement(new Boolean(false));
 
+		// set the values for vector for tile 17
 		Vector tile17 = new Vector(5);
 		tile17.addElement(new String("PURPLE"));
 		tile17.addElement(new String("PURPLE"));
 		tile17.addElement(new String("RED"));
 		tile17.addElement(new String("GREEN"));
 		tile17.addElement(new Boolean(true));
-
+		
+		// set the values for vector for tile 18
 		Vector tile18 = new Vector(5);
 		tile18.addElement(new String("BLUE"));
 		tile18.addElement(new String("RED"));
@@ -177,6 +203,7 @@ public class LakeTiles {
 		tile18.addElement(new String("GREEN"));
 		tile18.addElement(new Boolean(false));
 
+		// set the values for vector for tile 19
 		Vector tile19 = new Vector(4);
 		tile19.addElement(new String("BLUE"));
 		tile19.addElement(new String("BLACK"));
@@ -184,6 +211,7 @@ public class LakeTiles {
 		tile19.addElement(new String("GREEN"));
 		tile19.addElement(new Boolean(false));
 
+		// set the values for vector for tile 20
 		Vector tile20 = new Vector(4);
 		tile20.addElement(new String("RED"));
 		tile20.addElement(new String("GREY"));
@@ -191,6 +219,7 @@ public class LakeTiles {
 		tile20.addElement(new String("ORANGE"));
 		tile20.addElement(new Boolean(true));
 
+		// set the values for vector for tile 21
 		Vector tile21 = new Vector(4);
 		tile21.addElement(new String("ORANGE"));
 		tile21.addElement(new String("BLUE"));
@@ -198,6 +227,7 @@ public class LakeTiles {
 		tile21.addElement(new String("ORANGE"));
 		tile21.addElement(new Boolean(true));
 
+		// set the values for vector for tile 22
 		Vector tile22 = new Vector(4);
 		tile22.addElement(new String("RED"));
 		tile22.addElement(new String("GREY"));
@@ -205,6 +235,7 @@ public class LakeTiles {
 		tile22.addElement(new String("PURPLE"));
 		tile22.addElement(new Boolean(true));
 
+		// set the values for vector for tile 23
 		Vector tile23 = new Vector(4);
 		tile23.addElement(new String("PURPLE"));
 		tile23.addElement(new String("BLUE"));
@@ -212,6 +243,7 @@ public class LakeTiles {
 		tile23.addElement(new String("GREEN"));
 		tile23.addElement(new Boolean(true));
 
+		// set the values for vector for tile 24
 		Vector tile24 = new Vector(4);
 		tile24.addElement(new String("BLUE"));
 		tile24.addElement(new String("RED"));
@@ -219,6 +251,7 @@ public class LakeTiles {
 		tile24.addElement(new String("GREY"));
 		tile24.addElement(new Boolean(false));
 
+		// set the values for vector for tile 25
 		Vector tile25 = new Vector(4);
 		tile25.addElement(new String("BLUE"));
 		tile25.addElement(new String("WHITE"));
@@ -226,6 +259,7 @@ public class LakeTiles {
 		tile25.addElement(new String("PURPLE"));
 		tile25.addElement(new Boolean(false));
 
+		// set the values for vector for tile 26
 		Vector tile26 = new Vector(4);
 		tile26.addElement(new String("GREY"));
 		tile26.addElement(new String("WHITE"));
@@ -233,6 +267,7 @@ public class LakeTiles {
 		tile26.addElement(new String("WHITE"));
 		tile26.addElement(new Boolean(true));
 
+		// set the values for vector for tile 27
 		Vector tile27 = new Vector(4);
 		tile27.addElement(new String("PURPLE"));
 		tile27.addElement(new String("GREY"));
@@ -240,6 +275,7 @@ public class LakeTiles {
 		tile27.addElement(new String("ORANGE"));
 		tile27.addElement(new Boolean(false));
 
+		// set the values for vector for tile 28
 		Vector tile28 = new Vector(4);
 		tile28.addElement(new String("ORANGE"));
 		tile28.addElement(new String("BLUE"));
@@ -247,6 +283,7 @@ public class LakeTiles {
 		tile28.addElement(new String("BLUE"));
 		tile28.addElement(new Boolean(true));
 
+		// set the values for vector for tile 29
 		Vector tile29 = new Vector(4);
 		tile29.addElement(new String("BLUE"));
 		tile29.addElement(new String("RED"));
@@ -254,6 +291,7 @@ public class LakeTiles {
 		tile29.addElement(new String("GREY"));
 		tile29.addElement(new Boolean(true));
 
+		// set the values for vector for tile 30
 		Vector tile30 = new Vector(4);
 		tile30.addElement(new String("RED"));
 		tile30.addElement(new String("ORANGE"));
@@ -261,6 +299,7 @@ public class LakeTiles {
 		tile30.addElement(new String("WHITE"));
 		tile30.addElement(new Boolean(false));
 
+		// set the values for vector for tile 31
 		Vector tile31 = new Vector(4);
 		tile31.addElement(new String("PURPLE"));
 		tile31.addElement(new String("PURPLE"));
@@ -268,6 +307,7 @@ public class LakeTiles {
 		tile31.addElement(new String("PURPLE"));
 		tile31.addElement(new Boolean(false));
 
+		// set the values for vector for tile 32
 		Vector tile32 = new Vector(4);
 		tile32.addElement(new String("BLUE"));
 		tile32.addElement(new String("WHITE"));
@@ -275,6 +315,7 @@ public class LakeTiles {
 		tile32.addElement(new String("RED"));
 		tile32.addElement(new Boolean(false));
 
+		// set the values for vector for tile 33
 		Vector tile33 = new Vector(4);
 		tile33.addElement(new String("RED"));
 		tile33.addElement(new String("WHITE"));
@@ -282,6 +323,7 @@ public class LakeTiles {
 		tile33.addElement(new String("ORANGE"));
 		tile33.addElement(new Boolean(false));
 
+		// set the values for vector for tile 34
 		Vector tile34 = new Vector(4);
 		tile34.addElement(new String("ORANGE"));
 		tile34.addElement(new String("GREY"));
@@ -289,6 +331,7 @@ public class LakeTiles {
 		tile34.addElement(new String("RED"));
 		tile34.addElement(new Boolean(false));
 
+		// set the values for vector for tile 35
 		Vector tile35 = new Vector(4);
 		tile35.addElement(new String("BLUE"));
 		tile35.addElement(new String("PURPLE"));
@@ -296,6 +339,7 @@ public class LakeTiles {
 		tile35.addElement(new String("ORANGE"));
 		tile35.addElement(new Boolean(false));
 
+		// set the values for vector for tile 36
 		Vector tile36 = new Vector(4);
 		tile36.addElement(new String("WHITE"));
 		tile36.addElement(new String("GREEN"));
@@ -306,6 +350,7 @@ public class LakeTiles {
 	}
 
 	/**
+	 * getter for tiles id
 	 * @return the tilesId
 	 */
 	public int getTilesId() {
@@ -313,6 +358,7 @@ public class LakeTiles {
 	}
 
 	/**
+	 * setter for tiles id
 	 * @param tilesId the tilesId to set
 	 */
 	public void setTilesId(int tilesId) {
@@ -320,6 +366,7 @@ public class LakeTiles {
 	}
 
 	/**
+	 * getter for tiles id for the top tile
 	 * @return the topTileId
 	 */
 	public int getTopTileId() {
@@ -330,6 +377,7 @@ public class LakeTiles {
 
 
 	/**
+	 * setter for tiles id for the top tile
 	 * @param topTileId the topTileId to set
 	 */
 	public void setTopTileId(int topTileId) {
@@ -337,9 +385,8 @@ public class LakeTiles {
 	}
 
 
-
-
 	/**
+	 * getter for tiles id for the bottom tile
 	 * @return the bottomTileId
 	 */
 	public int getBottomTileId() {
@@ -347,9 +394,8 @@ public class LakeTiles {
 	}
 
 
-
-
 	/**
+	 * setter for tiles id for the bottom tile
 	 * @param bottomTileId the bottomTileId to set
 	 */
 	public void setBottomTileId(int bottomTileId) {
@@ -357,9 +403,8 @@ public class LakeTiles {
 	}
 
 
-
-
 	/**
+	 * getter for tiles id for the left tile
 	 * @return the leftTileId
 	 */
 	public int getLeftTileId() {
@@ -367,9 +412,8 @@ public class LakeTiles {
 	}
 
 
-
-
 	/**
+	 * setter for tiles id for the left tile
 	 * @param leftTileId the leftTileId to set
 	 */
 	public void setLeftTileId(int leftTileId) {
@@ -377,9 +421,8 @@ public class LakeTiles {
 	}
 
 
-
-
 	/**
+	 * getter for tiles id for the right tile
 	 * @return the rightTileId
 	 */
 	public int getRightTileId() {
@@ -387,9 +430,8 @@ public class LakeTiles {
 	}
 
 
-
-
 	/**
+	 * setter for tiles id for the right tile
 	 * @param rightTileId the rightTileId to set
 	 */
 	public void setRightTileId(int rightTileId) {
@@ -397,18 +439,17 @@ public class LakeTiles {
 	}
 
 
-
-
 	/**
-	 * @return the topColor
+	 * getter for the top color for the tile
+	 * @return the topColor, string type
 	 */
 	public String getTopColor() {
 		return topColor;
 	}
 
 
-
 	/**
+	 * setter for the top color for the tile
 	 * @param topColor the topColor to set
 	 */
 	public void setTopColor(String topColor) {
@@ -418,7 +459,8 @@ public class LakeTiles {
 
 
 	/**
-	 * @return the leftColor
+	 * getter for the left color for the tile
+	 * @return the leftColor, string type
 	 */
 	public String getLeftColor() {
 		return leftColor;
@@ -427,6 +469,7 @@ public class LakeTiles {
 
 
 	/**
+	 * setter for the left color for the tile
 	 * @param leftColor the leftColor to set
 	 */
 	public void setLeftColor(String leftColor) {
@@ -436,7 +479,8 @@ public class LakeTiles {
 
 
 	/**
-	 * @return the rightColor
+	 * getter for the right color for the tile
+	 * @return the rightColor, string type
 	 */
 	public String getRightColor() {
 		return rightColor;
@@ -445,6 +489,7 @@ public class LakeTiles {
 
 
 	/**
+	 * setter for the right color for the tile
 	 * @param rightColor the rightColor to set
 	 */
 	public void setRightColor(String rightColor) {
@@ -454,6 +499,7 @@ public class LakeTiles {
 
 
 	/**
+	 * getter for the bottom color for the tile
 	 * @return the bottomColor
 	 */
 	public String getBottomColor() {
@@ -463,6 +509,7 @@ public class LakeTiles {
 
 
 	/**
+	 * setter for the bottom color for the tile
 	 * @param bottomColor the bottomColor to set
 	 */
 	public void setBottomColor(String bottomColor) {
@@ -470,8 +517,8 @@ public class LakeTiles {
 	}
 
 
-
 	/**
+	 * getter method for the platform for the tile
 	 * @return the platform
 	 */
 	public boolean isPlatform() {
@@ -479,13 +526,12 @@ public class LakeTiles {
 	}
 
 
-
 	/**
+	 * setter method for the platform for the tile
 	 * @param platform the platform to set
 	 */
 	public void setPlatform(boolean platform) {
 		this.platform = platform;
 	}
-	
 	
 }
