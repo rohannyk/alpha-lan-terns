828f57b02387c2d4958fca4258e3932671f15bd5
savpreet <savpreetkaur1989@gmail.com>
2015-07-15 16:03:09 -0400

    Updated GameConfiguration and TestComponents

--- src/edu/concordia/app/test/TestComponents.java
+++ src/edu/concordia/app/test/TestComponents.java
@@ -43,15 +43,24 @@ public class TestComponents {
 	}
 		
 	@Test
+	/**
+	 * The test checks the FavourTokens 
+	 */
 	public void testFavourTokens() {
 	
 		
 		assertEquals(gc.TOTAL_FAVOR_TOKEN, gi.getGameFavorToken());
+		assertFalse(gi.getGameFavorToken()==10);
+		assertNotNull(gi.getGameFavorToken());
+		
 		//System.out.println(gi.getGameFavorToken());
 		
 	}
 		@Test
-		public void testConfigWithParam4() {
+		/**
+		 * The test checks the number of Draw tile stack with 4 players
+		 */
+		public void testnoOfDrawTileInStackWithParamFour() {
 			
 			
 			GameConfiguration c4 = new GameConfiguration(4);
@@ -60,45 +69,71 @@ public class TestComponents {
 		
 			
 			assertEquals(c4.noOfDrawTileInStack, gi4.gameTilesDrawPile.size());
+			assertFalse(gi4.gameTilesDrawPile.size()==10);
+			assertNotNull(gi4.gameTilesDrawPile.size());
 			
 		}
 	
 		@Test
-		public void testConfigWithParam3() {
+		/**
+		 * The test checks the number of Draw tile stack with 3 players
+		 */
+		public void testnoOfDrawTileInStackWithParamThree() {
 			
 			
 			
 		GameConfiguration c3 = new GameConfiguration(3);
         GameInstance gi3 = new GameInstance(c3);
 	    assertEquals(c3.noOfDrawTileInStack, gi3.gameTilesDrawPile.size());
+		assertFalse(gi3.gameTilesDrawPile.size()==10);
+		assertNotNull(gi3.gameTilesDrawPile.size());
+		
 		}
 		
 		
+		
 		@Test
-		public void testConfigWithParam2() {
+		/**
+		 * The test checks the number of Draw tile stack with 2 players
+		 */
+		public void testnoOfDrawTileInStackWithParamTwo() {
 			
 		GameConfiguration c2 = new GameConfiguration(2);
 	    GameInstance gi2 = new GameInstance(c2);
 	   	assertEquals(c2.noOfDrawTileInStack, gi2.gameTilesDrawPile.size());
+		assertFalse(gi2.gameTilesDrawPile.size()==10);
+		assertNotNull(gi2.gameTilesDrawPile.size());
+		
 		       
 		}
 		
 		
 		@Test
-		public void testConfigWithLantern3()
+		/**
+		 * The test checks the number of Lantern cards of each color with 4 players
+		 */
+		public void testNoOfLanternCardsForEveryColorWithParamThree()
 			
 		{
 			
 		GameConfiguration c3= new GameConfiguration(3);
 	    GameInstance gi3= new GameInstance(c3);
 	    
+	    
 	  assertEquals(c3.NUM_OF_LANTERN_CARDS_FOR_EVERY_COLOR, gi3.defaultLanternCardSize);
+		assertFalse(gi3.defaultLanternCardSize==10);
+		assertNotNull(gi3.defaultLanternCardSize);
+		
 		
 	    //System.out.println(c3.NUM_OF_LANTERN_CARDS_FOR_EVERY_COLOR);
 		}
 		
+	
 		@Test
-		public void testConfigWithLantern4()
+		/**
+		 * The test checks the number of Lantern cards of each color with 4 players
+		 */
+		public void testNoOfLanternCardsForEveryColorWithParamFour()
 			
 		{
 			
@@ -106,12 +141,18 @@ public class TestComponents {
 	    GameInstance gi4= new GameInstance(c4);
 	    
 	  assertEquals(c4.NUM_OF_LANTERN_CARDS_FOR_EVERY_COLOR, gi4.defaultLanternCardSize);
+	  assertFalse(gi4.defaultLanternCardSize==10);
+		assertNotNull(gi4.defaultLanternCardSize);
+		
 		
 	    //System.out.println(c4.NUM_OF_LANTERN_CARDS_FOR_EVERY_COLOR);
 		}
 		
 		@Test
-		public void testConfigWithLantern2()
+		/**
+		 * The test checks the number of Lantern cards of each color with 2 players
+		 */
+		public void testNoOfLanternCardsForEveryColorWithParamTwo()
 			
 		{
 			
@@ -119,6 +160,9 @@ public class TestComponents {
 	    GameInstance gi2= new GameInstance(c2);
 	    
 	  assertEquals(c2.NUM_OF_LANTERN_CARDS_FOR_EVERY_COLOR, gi2.defaultLanternCardSize);
+	  assertFalse(gi2.defaultLanternCardSize==10);
+		assertNotNull(gi2.defaultLanternCardSize);
+		
 		
 	   // System.out.println(c2.NUM_OF_LANTERN_CARDS_FOR_EVERY_COLOR);
 		}
