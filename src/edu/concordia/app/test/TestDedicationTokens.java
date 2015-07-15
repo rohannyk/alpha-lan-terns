7ec0ab7b34af4cb1ab73fc07ca6770a530d544f6
abhijit159 <patil.abhijit159@gmail.com>
2015-07-15 14:17:09 -0400

    updated TestDedicationTokens

--- src/edu/concordia/app/test/TestDedicationTokens.java
+++ src/edu/concordia/app/test/TestDedicationTokens.java
@@ -49,49 +49,7 @@ public class TestDedicationTokens {
 		DedicationTokens dedicationTokensActual= new DedicationTokens();
 		dedicationTokensActual= gc1.initializeDedicationTokens(2);
 		
-		/*Vector<Integer> dedicationTokenFour = new Vector<Integer>();
-
-		Vector<Integer> dedicationTokenSix = new Vector<Integer>();
-
-		Vector<Integer> dedicationTokenSeven = new Vector<Integer>();
-
-		Vector<Integer> genericDedicationTokens = new Vector<Integer>();
-
-			// dedication token four
-			dedicationTokenFour.add(8);
-			dedicationTokenFour.add(7);
-			dedicationTokenFour.add(6);
-			dedicationTokenFour.add(5);
-			dedicationTokenFour.add(5);
-			dedicationTokenFour.add(4);
-			
-			// dedication token six
-			dedicationTokenSix.add(9);
-			dedicationTokenSix.add(8);
-			dedicationTokenSix.add(7);
-			dedicationTokenSix.add(6);
-			dedicationTokenSix.add(5);
-			dedicationTokenSix.add(5);
-
-			// dedication token seven
-			dedicationTokenSeven.add(10);
-			dedicationTokenSeven.add(9);
-			dedicationTokenSeven.add(8);
-			dedicationTokenSeven.add(7);
-			dedicationTokenSeven.add(6);
-			dedicationTokenSeven.add(5);
-			
-			// generic dedication token
-			genericDedicationTokens.add(4);
-			genericDedicationTokens.add(4);
-			genericDedicationTokens.add(4);
-			
-			DedicationTokens dedicationTokensExpected = new DedicationTokens(dedicationTokenFour, dedicationTokenSix, 
-					dedicationTokenSeven, genericDedicationTokens);
-			
-			assertEquals(dedicationTokensExpected,dedicationTokensActual);*/
-		
-			
+				
 			Vector<Integer> dedicationTokenFour = new Vector<Integer>(dedicationTokensActual.getDedicationTokenFour());
 			int sizeX=dedicationTokenFour.size();
 			assertEquals(sizeX,6);
