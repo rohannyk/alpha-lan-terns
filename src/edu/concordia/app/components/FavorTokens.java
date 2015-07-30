d9634a5f8d4f705de0312597776cb805f7f18f80
salehahmedca <sachchabd@gmail.com>
2015-07-30 17:17:01 -0400

    Commented FavorTokens
    Modified for JavaDoc

--- src/edu/concordia/app/components/FavorTokens.java
+++ src/edu/concordia/app/components/FavorTokens.java
@@ -7,58 +7,64 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 
 /**
- * This class creates Favor Tokens for the players to use in the game
- * Favor Tokens are used by the player to exchange lantern cards 
+ * This class creates Favor Tokens for the players to use in the game Favor
+ * Tokens are used by the player to exchange lantern cards
+ * 
  * @author Team E
  */
 public class FavorTokens {
-	
+
 	// variable to count favor tokens initialized to 0
 	public int favtokenCount = 0;
-	
+
 	/**
 	 * Default Constructor
 	 */
-	public FavorTokens(){
-		
+	public FavorTokens() {
+
 	}
-	
+
 	/**
-	 * Constructor
+	 * Constructor to set favor token count
 	 */
-	public FavorTokens(int favtokenCount){
+	public FavorTokens(int favtokenCount) {
 		this.favtokenCount = favtokenCount;
 	}
-	
-	
-	@XmlAttribute(name="totalFavorTokens")
+
+	@XmlAttribute(name = "totalFavorTokens")
 	/**
-	 * @return the favtokenCount
+	 * This method gets the favor token count.
+	 * @return the favtokenCount: int type
 	 */
 	public int getFavtokenCount() {
 		return favtokenCount;
 	}
 
-
 	/**
-	 * @param favtokenCount the favtokenCount to set
+	 * This method sets the favor token count.
+	 * @param favtokenCount: int type
+	 *            the favtokenCount to set
 	 */
 	public void setFavtokenCount(int favtokenCount) {
 		this.favtokenCount = favtokenCount;
 	}
-	
+
 	/**
-	 * @param tokens, the favor tokens added for players to favtokenCount 
+	 * This method adds/increases the favor token.
+	 * @param tokens,
+	 *            the favor tokens added for players to favtokenCount
 	 */
-	public void addFavorToken(int tokens){
-		favtokenCount = favtokenCount + tokens; 
+	public void addFavorToken(int tokens) {
+		favtokenCount = favtokenCount + tokens;
 	}
-	
+
 	/**
-	 * @param tokens, the favor tokens removed for players from favtokenCount 
+	 * This method removes/decreases the favor token.
+	 * @param tokens,
+	 *            the favor tokens removed for players from favtokenCount
 	 */
-	public void removeFavorToken(int tokens){
-		favtokenCount = favtokenCount - tokens; 
+	public void removeFavorToken(int tokens) {
+		favtokenCount = favtokenCount - tokens;
 	}
 
 }
