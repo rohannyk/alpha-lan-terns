f165d39bffeaf8da19f00cd16d80173f973cb992
salehahmedca <sachchabd@gmail.com>
2015-07-30 17:42:31 -0400

    Commented StartPlayerMarker
    Commented StartPlayerMarker for JavaDoc.

--- src/edu/concordia/app/components/StartPlayerMarker.java
+++ src/edu/concordia/app/components/StartPlayerMarker.java
@@ -4,35 +4,35 @@
 package edu.concordia.app.components;
 
 /**
- * The start marker is given to the player who has the start lake tile as red pointing towards him
+ * The start marker is given to the player who has the start lake tile as red
+ * pointing towards him
+ * 
  * @author Team E
  *
  */
 public class StartPlayerMarker {
-	
-//	public int startMarker;
-	
+
 	/**
-	 * The start marker tile given to player.
-	 * True - Player has the tile.
-	 * False - Player don't have the tile.
+	 * The start marker tile given to player. True - Player has the tile. False
+	 * - Player don't have the tile.
 	 */
 	public boolean startMarker;
 
 	/**
-	 * @return the startMarker
+	 * This method gets the boolean for startMarker 
+	 * @return the startMarker: boolean type.
 	 */
 	public boolean isStartMarker() {
 		return startMarker;
 	}
 
 	/**
-	 * @param startMarker the startMarker to set
+	 * This method sets the boolean for startMarker
+	 * @param startMarker: boolean type.
+	 *            the startMarker to set
 	 */
 	public void setStartMarker(boolean startMarker) {
 		this.startMarker = startMarker;
 	}
-	
-	
 
 }
