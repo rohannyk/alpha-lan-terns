23cda94512d57104cd9872392689d367a747f7d6
salehahmedca <sachchabd@gmail.com>
2015-07-30 13:05:21 -0400

    Adding FileMangement class to manage the save and load files.

---
+++ src/edu/concordia/app/model/FileManagement.java
@@ -0,0 +1,148 @@
+/**
+ * 
+ */
+package edu.concordia.app.model;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import edu.concordia.app.model.GameInstance;
+
+/**
+ * FileManagement class to manage the save and load files.
+ * @author Team E
+ *
+ */
+public class FileManagement {
+	
+	/**
+	 * The instance variable of GameConfiguration class.
+	 */
+	private GameConfiguration gameConfig;
+	
+	/**
+	 * The instance variable of GameInstance class.
+	 */
+	private GameInstance gameInstance;
+
+	/**
+	 * The default constructor of GameController class.
+	 */
+	public FileManagement() {
+		
+	}
+
+	/**
+	 * This GameController class constructor will set 
+	 * GameConfiguration and GameInstance objects.
+	 * @param gameConfig The GameConfiguration class object.
+	 * @param gameInstance The GameInstance class object.
+	 */
+	public FileManagement(GameConfiguration gameConfig,
+			GameInstance gameInstance) {
+		
+		this.gameConfig = gameConfig;
+		this.gameInstance = gameInstance;
+	}
+	
+	/**
+	 * This method will check if the file name is empty or not.
+	 * If file name is empty, Default file name will be assigned.
+	 * @param gameFile The name of the file to save.
+	 */
+	public void saveGameToFile(String fileName){
+		File file;
+		
+		
+		if(fileName == null || fileName == ""){
+			file = new File("/Users/lovepreet/default_game_save.xml");
+		}
+		else{
+			file = new File(fileName);
+		}
+		
+		saveGameToXml(file);
+	}
+	
+	/**
+	 * This method will save GameInstance class object to an 'XML' file.
+	 * @param gameFile The file name of the save file.
+	 */
+	private void saveGameToXml(File gameFile){
+		try {
+			JAXBContext context = JAXBContext.newInstance(GameInstance.class);
+			Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			marshaller.marshal(this.gameInstance, gameFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * This method will check if the file name is empty or not.
+	 * @param fileName The name of the file to save.
+	 * @return The GameInstance class object will be returned.
+	 */
+	public GameInstance loadGameFromFile(String fileName) {
+		File file = null;
+		
+		if(fileName == null || fileName == ""){
+			
+			System.out.println("File Name is empty. Please enter the file name.");
+			
+			return null;
+		}
+		else{
+			file = new File(fileName);
+			
+			boolean fileExists = checkFileExist(file);
+			
+			if(fileExists){
+				return loadGameState(file);
+			}else{
+				return null;
+			}
+		}
+		
+	}
+	
+	/**
+	 * This method checks if the given file exists or not.
+	 * @param file of File type.
+	 * @return The boolean value to check if file exists or not.
+	 */
+	private boolean checkFileExist(File file){
+		
+		return file.exists();
+	}
+	
+	/**
+	 * This method will load 'XML' file and get the GameInstance class object.
+	 * @param file The file name of the file to be loaded.
+	 * @return The GameInstance class object will be returned.
+	 */
+	private GameInstance loadGameState(File file) {
+		
+		System.out.println("Loading the game.......!");
+		
+		GameInstance gameInstance = null;
+		try {
+			JAXBContext context = JAXBContext.newInstance(GameInstance.class);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			gameInstance = (GameInstance) unmarshaller.unmarshal(file);
+		} catch (Exception e) { 
+			e.printStackTrace();
+		}
+		
+		this.gameInstance = gameInstance;
+		
+		System.out.println("Game Loaded successfully.");
+		
+		return gameInstance;
+	}
+
+}
