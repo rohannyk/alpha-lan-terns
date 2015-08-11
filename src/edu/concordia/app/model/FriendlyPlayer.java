10734bcc96a294b8d51224e92cd790e28a090b41
Rohannayak <rohannyk@live.com>
2015-08-11 17:02:17 -0400

    Committed FriedlyPlayer.java

---
+++ src/edu/concordia/app/model/FriendlyPlayer.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package edu.concordia.app.model;
+
+/**
+ * @author lovepreet
+ *
+ */
+public class FriendlyPlayer extends Players {
+	public static Players makeAMove( Players playing, GameInstance gameObjs, String option)
+	{
+		return playing;
+	}
+}
