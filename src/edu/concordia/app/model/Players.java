90b7db6907a3ca9c514abbc3440eef713fc394e7
abhijit159 <patil.abhijit159@gmail.com>
2015-08-11 16:56:32 -0400

    Updated Players

--- src/edu/concordia/app/model/Players.java
+++ src/edu/concordia/app/model/Players.java
@@ -3,17 +3,22 @@
  */
 package edu.concordia.app.model;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Vector;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+
 import edu.concordia.app.components.DedicationTokens;
 import edu.concordia.app.components.LakeTiles;
 import edu.concordia.app.components.LanternCards;
 import edu.concordia.app.components.LanternCards.Color;
 import edu.concordia.app.components.Tokens;
+import edu.concordia.app.strategy.PlayerStrategy;
+import edu.concordia.app.view.LanternGameView;
 
 @XmlRootElement
 /**
@@ -23,7 +28,25 @@ import edu.concordia.app.components.Tokens;
  *
  */
 public class Players {
-
+	
+	public ArrayList<String> possiblePlayerColorExchangeMoves = new ArrayList<String>();
+	
+	public ArrayList<String> possibleGameColorExchangeMoves = new ArrayList<String>();
+	
+	public ArrayList<String> possibleExchangeMoves = new ArrayList<String>();
+	
+	public ArrayList<String> possibleDedicationFourUniqueColor = new ArrayList<String>();
+	
+	public ArrayList<String> possibleDedicationThreePairColor = new ArrayList<String>();
+	
+	public ArrayList<String> possibleDedicationSevenUniqueColor = new ArrayList<String>();
+	
+	public ArrayList<String> firstLakeTilePossibleScore = new ArrayList<String>();
+	
+	public ArrayList<String> secondLakeTilePossibleScore = new ArrayList<String>();
+	
+	public ArrayList<String> thirdLakeTilePossibleScore = new ArrayList<String>();
+	
 	private GameConfiguration config;
 
 	/**
@@ -142,6 +165,8 @@ public class Players {
 	 * The number of favor tokens holded by player.
 	 */
 	private int playerFavorToken;
+	
+	private PlayerStrategy strategy;
 
 	/**
 	 * Default constructor of Players class Initialize all variables and values
@@ -519,7 +544,7 @@ public class Players {
 	 * @return the totalPoints: int type
 	 */
 	public int getTotalPoints() {
-		//int totalPoints = 0;
+		int totalPoints = 0;
 
 		Vector<Integer> dediFourToken = getDedicationTokens()
 				.getDedicationTokenFour();
@@ -529,6 +554,7 @@ public class Players {
 				.getDedicationTokenSeven();
 		Vector<Integer> genericToken = getDedicationTokens()
 				.getGenericDedicationTokens();
+		
 
 		if (!dediFourToken.isEmpty()) {
 			for (Iterator<Integer> iterator = dediFourToken.iterator(); 
@@ -557,7 +583,7 @@ public class Players {
 			}
 		}
 
-		if (!dediFourToken.isEmpty()) {
+		if (!genericToken.isEmpty()) {
 			for (Iterator<Integer> iterator = genericToken.iterator(); iterator
 					.hasNext();) {
 				Integer integer = (Integer) iterator.next();
@@ -649,7 +675,7 @@ public class Players {
 								.getDedicationTokenFour();
 						if (dedicationVal.size() > 0) {
 							int dedicationValue = dedicationVal.remove(0);
-							DedicationTokens playerDedicationObj = getDedicationTokens();
+							DedicationTokens playerDedicationObj = this.getDedicationTokens();
 							playerDedicationObj.getDedicationTokenFour().add(
 									dedicationValue);
 							if (dedicationVal.isEmpty()) {
@@ -663,7 +689,7 @@ public class Players {
 									.getGenericDedicationTokens();
 							if (genDedicationVal.size() > 0) {
 								int genericValue = genDedicationVal.remove(0);
-								DedicationTokens playerDedicationObj = getDedicationTokens();
+								DedicationTokens playerDedicationObj = this.getDedicationTokens();
 								playerDedicationObj
 										.getGenericDedicationTokens().add(
 												genericValue);
@@ -763,7 +789,7 @@ public class Players {
 								.getDedicationTokenSix();
 						if (dedicationSixVal.size() > 0) {
 							int dedicationSixValue = dedicationSixVal.remove(0);
-							DedicationTokens playerDedicationObj = getDedicationTokens();
+							DedicationTokens playerDedicationObj = this.getDedicationTokens();
 							playerDedicationObj.getDedicationTokenSix().add(
 									dedicationSixValue);
 							if (dedicationSixVal.isEmpty()) {
@@ -777,7 +803,7 @@ public class Players {
 									.getGenericDedicationTokens();
 							if (genDedicationVal.size() > 0) {
 								int genericValue = genDedicationVal.remove(0);
-								DedicationTokens playerDedicationObj = getDedicationTokens();
+								DedicationTokens playerDedicationObj = this.getDedicationTokens();
 								playerDedicationObj
 										.getGenericDedicationTokens().add(
 												genericValue);
@@ -840,7 +866,7 @@ public class Players {
 							if (dedicationSevenVal.size() > 0) {
 								int dedicationSevenValue = dedicationSevenVal
 										.remove(0);
-								DedicationTokens playerDedicationObj = getDedicationTokens();
+								DedicationTokens playerDedicationObj = this.getDedicationTokens();
 								playerDedicationObj.getDedicationTokenSeven()
 										.add(dedicationSevenValue);
 								if (dedicationSevenVal.isEmpty()) {
@@ -854,7 +880,7 @@ public class Players {
 										.getGenericDedicationTokens();
 								if (genGenericVal.size() > 0) {
 									int genericValue = genGenericVal.remove(0);
-									DedicationTokens playerDedicationObj = getDedicationTokens();
+									DedicationTokens playerDedicationObj = this.getDedicationTokens();
 									playerDedicationObj
 											.getGenericDedicationTokens().add(
 													genericValue);
@@ -872,14 +898,14 @@ public class Players {
 		}
 	}
 
+	//updated method
 	/**
 	 * The method prompt player to enter lantern card color player wants to exchange and then perform the exchange operation.
 	 * @param gameObj The GameInstance object to update its elements according to lantern card exchange.
 	 * @param scan
 	 * @param playGame
 	 */
-	public void exchageLaternCard(GameInstance gameObj, Scanner scan,
-			PlayGame playGame) {
+	public void exchageLaternCard(GameInstance gameObj, Scanner scan) {
 		boolean loop = true;
 		String yourColor = "";
 		String gameColor = "";
@@ -924,7 +950,11 @@ public class Players {
 					+ gameObj.getGameRedLanternCardCount());
 			System.out.println("Which your color do you want to give: ");
 			yourColor = scan.next();
-			loop = playGame.lanternColorPlayerValidation(this, yourColor);
+			
+			//check if game has one lantern card with player for exchange
+//			loop = playGame.lanternColorPlayerValidation(this, yourColor);
+			loop = this.lanternColorPlayerValidation(yourColor);
+			
 			if (loop == true) {
 				loop = false;
 			} else {
@@ -934,8 +964,13 @@ public class Players {
 				continue;
 			}
 			System.out.println("Which Game color do you want to get: ");
+			
 			gameColor = scan.next();
-			loop = playGame.lanternColorGameValidation(gameObj, gameColor);
+			
+			//check if game has one lantern card in game for exchange
+//			loop = playGame.lanternColorGameValidation(gameObj, gameColor);
+			loop = gameObj.lanternColorGameValidation(gameColor);
+			
 			if (loop == true) {
 				loop = false;
 			} else {
@@ -946,10 +981,18 @@ public class Players {
 			}
 		}
 		if (!loop) {
+			
 			setPlayerFavorToken(getPlayerFavorToken() - 2);
+			
 			gameObj.setGameFavorToken(gameObj.getGameFavorToken() + 2);
-			playGame.playerColorAugment(yourColor, this, gameColor);
-			playGame.gameColorAugment(gameColor, gameObj, yourColor);
+			
+			//method to remove lantern card color from player and add to game
+			//playGame.playerColorAugment(yourColor, this, gameColor);
+			this.playerColorAugment(yourColor, gameColor);
+			
+			//method to remove lantern card color from game and add to player
+			//playGame.gameColorAugment(gameColor, gameObj, yourColor);
+			gameObj.gameColorAugment(gameColor, yourColor);
 		}
 	}
 
@@ -1116,6 +1159,326 @@ public class Players {
 				}
 			}
 		}
+		
 	}
 
+	public void playMove(Players playing, GameInstance gameObjs, String option)
+	{
+		if(playing instanceof GreedyPlayer)
+		{
+			playing = GreedyPlayer.makeAMove(playing, gameObjs, option);
+		}else if (playing instanceof UnfriendlyPlayer)
+		{
+			playing = UnfriendlyPlayer.makeAMove(playing, gameObjs, option); 
+		}else if (playing instanceof RandomPlayer)
+		{
+			playing = RandomPlayer.makeAMove(playing, gameObjs, option);
+		}else if(playing instanceof FriendlyPlayer)
+		{
+			playing = FriendlyPlayer.makeAMove(playing, gameObjs, option);
+		}else if(playing instanceof HumanPlayer)
+		{
+			playing = HumanPlayer.makeAMove(playing, gameObjs, option);
+		}
+		
+	}
+
+	/**
+	 * The method to validate the options(exchange/make a dedication) user can choose during the game.
+	 * @param gameObj The GameInstance to verify if exchange or is possible or not.
+	 * @return  The string containing the possible options.
+	 */
+	public String genValidation(GameInstance gameObj) {
+		boolean exahange = false;
+		String retStr = "";
+		if (getPlayerFavorToken() >= 2) {
+			if (getPlayerBlackLanternCardCount() > 0
+					|| getPlayerWhiteLanternCardCount() > 0
+					|| getPlayerBlueLanternCardCount() > 0
+					|| getPlayerGreenLanternCardCount() > 0
+					|| getPlayerOrangeLanternCardCount() > 0
+					|| getPlayerPurpleLanternCardCount() > 0
+					|| getPlayerRedLanternCardCount() > 0) {
+				if (gameObj.getGameBlackLanternCardCount() > 0
+						|| gameObj.getGameWhiteLanternCardCount() > 0
+						|| gameObj.getGameBlueLanternCardCount() > 0
+						|| gameObj.getGameGreenLanternCardCount() > 0
+						|| gameObj.getGameOrangeLanternCardCount() > 0
+						|| gameObj.getGamePurpleLanternCardCount() > 0
+						|| gameObj.getGameRedLanternCardCount() > 0) {
+					retStr += "exchange";
+				}
+			}
+		}
+		boolean type1 = false;
+		if (getPlayerBlackLanternCardCount() > 3
+				|| getPlayerWhiteLanternCardCount() > 3
+				|| getPlayerBlueLanternCardCount() > 3
+				|| getPlayerGreenLanternCardCount() > 3
+				|| getPlayerOrangeLanternCardCount() > 3
+				|| getPlayerPurpleLanternCardCount() > 3
+				|| getPlayerRedLanternCardCount() > 3) {
+			DedicationTokens dedicationToken = gameObj.getDedicationTokens();
+			if (dedicationToken.getDedicationTokenFourSize() > 0) {
+				retStr += "type1";
+			} else if (dedicationToken.getGenericDedicationTokensSize() > 0) {
+				retStr += "type1";
+			}
+		}
+		boolean type2 = false;
+		int countPairs2 = 0;
+		if (getPlayerBlackLanternCardCount() > 1) {
+			countPairs2++;
+		}
+		if (getPlayerWhiteLanternCardCount() > 1) {
+			countPairs2++;
+		}
+		if (getPlayerBlueLanternCardCount() > 1) {
+			countPairs2++;
+		}
+		if (getPlayerGreenLanternCardCount() > 1) {
+			countPairs2++;
+		}
+		if (getPlayerOrangeLanternCardCount() > 1) {
+			countPairs2++;
+		}
+		if (getPlayerPurpleLanternCardCount() > 1) {
+			countPairs2++;
+		}
+		if (getPlayerRedLanternCardCount() > 1) {
+			countPairs2++;
+		}
+		if (countPairs2 > 2) {
+			DedicationTokens dedicationToken = gameObj.getDedicationTokens();
+			if (dedicationToken.getDedicationTokenSixSize() > 0) {
+				retStr += "type2";
+			} else if (dedicationToken.getGenericDedicationTokensSize() > 0) {
+				retStr += "type2";
+			}
+		}
+		boolean type3 = false;
+		int countPairs7 = 0;
+		if (getPlayerBlackLanternCardCount() > 0) {
+			countPairs7++;
+		}
+		if (getPlayerWhiteLanternCardCount() > 0) {
+			countPairs7++;
+		}
+		if (getPlayerBlueLanternCardCount() > 0) {
+			countPairs7++;
+		}
+		if (getPlayerGreenLanternCardCount() > 0) {
+			countPairs7++;
+		}
+		if (getPlayerOrangeLanternCardCount() > 0) {
+			countPairs7++;
+		}
+		if (getPlayerPurpleLanternCardCount() > 0) {
+			countPairs7++;
+		}
+		if (getPlayerRedLanternCardCount() > 0) {
+			countPairs7++;
+		}
+		if (countPairs7 > 6) {
+			DedicationTokens dedicationToken = gameObj.getDedicationTokens();
+			if (dedicationToken.getDedicationTokenSevenSize() > 0) {
+				retStr += "type3";
+			} else if (dedicationToken.getGenericDedicationTokensSize() > 0) {
+				retStr += "type3";
+			}
+		}
+		return retStr;
+	}
+	
+	/**
+	 * This method will discard the excess Lantern cards, if the current player
+	 * has more than 12 lantern cards. The player has two options, either to
+	 * discard the lantern cards of his choice or to perform either of the three
+	 * possible dedications.
+	 * 
+	 * @param gameObj
+	 *            The GameInstance object for updating game elements according
+	 *            to user choice.
+	 * @param playing
+	 *            The current player of the game.
+	 * @param opt
+	 *            The string contain options that player can choose
+	 *            (Dedication/discard).
+	 * @return The string contain those choices that are not used by the user.
+	 */
+	public static String filterExcessLanternCards(GameInstance gameObj,
+			Players playing, String opt) 
+	{
+		
+		/*while (playing.getLanternCardCount() > 12) 
+		{
+			System.out.println("---------- Lantern Cards"
+					+ " Count Check -----------");
+
+			System.out.println("Player" + playing.getPlayerNumber()
+					+ " has more than 12 lantern cards.");
+
+			System.out.println();
+
+			System.out.println("You have following choices to move forward.");
+
+			if (opt.contains("type1") || opt.contains("type2")
+					|| opt.contains("type3")) {
+				System.out.println("1: Make a dedication.");
+			}
+
+			System.out.println();
+
+			System.out.println("2: Discard Lantern Cards.");
+
+			System.out.println();
+
+			System.out.print("Please choose one option:");
+
+			int checkChoice = scan.nextInt();
+
+			switch (checkChoice) {
+			
+			case 1:
+				// make dedication
+				playing.makeADedication(gameObj, opt, scan);
+
+				// remove dedication substring from opt string
+				opt = removeSubstring(new String("type1"), opt);
+				opt = removeSubstring(new String("type2"), opt);
+				opt = removeSubstring(new String("type3"), opt);
+
+				break;
+
+			case 2:
+				
+				//discard the lantern cards.
+				playing.discardLanternCards(gameObj, scan);
+				break;
+
+			default:
+				break;
+			}
+		}*/
+		return opt;
+	}
+
+	/**
+	 * The method to remove the lake tile from player's hand.
+	 * 
+	 * @param tileInHand
+	 *            The lake tile to be removed.
+	 * @return The player of the game with update lake tiles in hand.
+	 */
+	public Players removePlacedLakeTile(LakeTiles tileInHand) {
+		Vector<LakeTiles> handLakeTiles = getCurrentLakeTilesHold();
+		
+		boolean removed = handLakeTiles.remove(tileInHand);
+	//		System.out.println("check removed "+ removed);
+	//		
+	//		System.out.println(" tiles in hand "+handLakeTiles.size());
+	//		System.out.println(" tiles in hand "+playing.getCurrentLakeTilesHold().size());
+		return this;
+	}
+
+	/**
+	 * The method to replenish lake tile from draw stack to the players hand,
+	 * after the player has placed the lake tile on the board.
+	 * 
+	 * @param gameObj
+	 *            The GameInstance object for updating the lantern cards of the
+	 *            game board.
+	 * @param playing
+	 *            The current player of the game.
+	 */
+	public static boolean replenishLakeTilesInHand(GameInstance gameObj,
+			Players playing) {
+		LakeTiles replenishedTile = null;
+	
+		// get top tile from draw pile and remove it.
+		if (gameObj.getGameTilesDrawPile().size() > 0) {
+			replenishedTile = gameObj.getGameTilesDrawPile().remove(0);
+	
+			// LakeTiles replenishedTile =
+			// gameObj.getGameTilesDrawPile().firstElement();
+	
+			// add tile to player hand
+			playing.getCurrentLakeTilesHold().add(replenishedTile);
+	
+			System.out.println("--------Replenished Lake Tile for Player "
+					+ playing.getPlayerNumber() + "--------");
+			
+			//display tile details
+			new LanternGameView().displayLakeTiles(gameObj, replenishedTile.getTilesId());
+	
+			System.out.println("---------------------------------------------");
+	
+			return true;
+		} else {
+			return false;
+		}
+	
+	}
+
+	/**
+	 * @return the strategy
+	 */
+	public PlayerStrategy getStrategy() {
+		return strategy;
+	}
+
+	/**
+	 * @param strategy the strategy to set
+	 */
+	public void setStrategy(PlayerStrategy strategy) {
+		this.strategy = strategy;
+	}
+
+	/**
+	 * @return the possibleDedicationFourUniqueColor
+	 */
+	public ArrayList<String> getPossibleDedicationFourUniqueColor() {
+		return possibleDedicationFourUniqueColor;
+	}
+
+	/**
+	 * @param possibleDedicationFourUniqueColor the possibleDedicationFourUniqueColor to set
+	 */
+	public void setPossibleDedicationFourUniqueColor(
+			ArrayList<String> possibleDedicationFourUniqueColor) {
+		this.possibleDedicationFourUniqueColor = possibleDedicationFourUniqueColor;
+	}
+
+	/**
+	 * @return the possibleDedicationThreePairColor
+	 */
+	public ArrayList<String> getPossibleDedicationThreePairColor() {
+		return possibleDedicationThreePairColor;
+	}
+
+	/**
+	 * @param possibleDedicationThreePairColor the possibleDedicationThreePairColor to set
+	 */
+	public void setPossibleDedicationThreePairColor(
+			ArrayList<String> possibleDedicationThreePairColor) {
+		this.possibleDedicationThreePairColor = possibleDedicationThreePairColor;
+	}
+
+	/**
+	 * @return the possibleDedicationSevenUniqueColor
+	 */
+	public ArrayList<String> getPossibleDedicationSevenUniqueColor() {
+		return possibleDedicationSevenUniqueColor;
+	}
+
+	/**
+	 * @param possibleDedicationSevenUniqueColor the possibleDedicationSevenUniqueColor to set
+	 */
+	public void setPossibleDedicationSevenUniqueColor(
+			ArrayList<String> possibleDedicationSevenUniqueColor) {
+		this.possibleDedicationSevenUniqueColor = possibleDedicationSevenUniqueColor;
+	}
+	
+	
+	
 }
