dc990dbc0a5915270b306b372af61e0cf78a7411
savpreet <savpreetkaur1989@gmail.com>
2015-08-13 18:07:10 -0400

    updated AllTests

--- src/edu/concordia/app/test/AllTests.java
+++ src/edu/concordia/app/test/AllTests.java
@@ -14,7 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({ TestDedicationTokens.class, TestFavorTokens.class, TestLakeTiles.class, 
-				TestPlayers.class,TestComponents.class,TestStartTile.class})
+				TestPlayers.class,TestComponents.class,TestStartTile.class, TestMakeDedicationTest.class})
 public class AllTests {
 
 }
