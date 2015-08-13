cf19de42d77724d8b379ba6a5e9a8315becca6c4
savpreet <savpreetkaur1989@gmail.com>
2015-08-13 17:56:24 -0400

    Updated UnfriendlyPlayerStrategy

--- src/edu/concordia/app/strategy/UnfriendlyPlayerStrategy.java
+++ src/edu/concordia/app/strategy/UnfriendlyPlayerStrategy.java
@@ -26,6 +26,8 @@ public class UnfriendlyPlayerStrategy extends PlayerStrategy implements Serializ
 
 	private static final long serialVersionUID = 8707943598805857983L;
 	private GameInstance gameObj;
+	
+	public String name = "Unfriendly";;
 
 	/**
 	 * Constructor
@@ -35,6 +37,7 @@ public class UnfriendlyPlayerStrategy extends PlayerStrategy implements Serializ
 	 */
 	public UnfriendlyPlayerStrategy(GameInstance gameObj) {
 		this.gameObj = gameObj;
+		super.name = "Unfriendly";
 	}
 
 	/**
