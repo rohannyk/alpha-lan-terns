9762899485dc3abdb3ba1b413cb3a0ffd7e0da6a
savpreet <savpreetkaur1989@gmail.com>
2015-08-13 18:00:46 -0400

    updated TestController

--- src/edu/concordia/app/test/TestController.java
+++ src/edu/concordia/app/test/TestController.java
@@ -7,14 +7,18 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Vector;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import edu.concordia.app.model.*;
 import edu.concordia.app.controller.*;
 import edu.concordia.app.main.LanternMain;
 import edu.concordia.app.components.*;
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 /**
@@ -26,12 +30,19 @@ public class TestController {
 	GameInstance gi;
 	GameConfiguration gc;
 	LanternMain lm;
+	Vector<String> playerTypes;
 	
 	@Before
 	public void setUp(){
 		gc = new GameConfiguration();
 		gi = new GameInstance();
 		lm = new LanternMain();
+		playerTypes = new Vector<String>();
+		
+		playerTypes.add("Greedy");
+		playerTypes.add("Random");
+		playerTypes.add("Friendly");
+		playerTypes.add("Human");
 	}
 	
 	@After
@@ -39,6 +50,7 @@ public class TestController {
 		gc = null;
 		gi = null;
 		lm = null;
+		playerTypes = null;
 	}
 
 	
@@ -49,7 +61,7 @@ public class TestController {
 	public void testNoOfPlayers() {
 
 		int noOfPlayers = lm.getInputNoOfPlayers();
-		gi = new GameInstance(new GameConfiguration(noOfPlayers));
+		gi = new GameInstance(new GameConfiguration(noOfPlayers), playerTypes);
 		
 		//assertTrue(gi.playersList.length == noOfPlayers);
 	}
